cf2118b0b6a56e271f00f79e9bded86457cd6711
Rafael Grimmler da Rocha <grimmlerr@gmail.com>
2022-05-12 00:17:13 -0300

    artistPageDone

--- src/Pages/Artist/index.js
+++ src/Pages/Artist/index.js
@@ -1,22 +1,86 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-import { Container, MainContentContainer, ArtistInformationsContainer, TracksContainer } from './styles';
+import { Container, MainContentContainer, ArtistInformationsContainer, TracksContainer, ArtistImage, ArtistImageContainer, ArtistImageShadow, ArtistNameContainer, TrackList } from './styles';
 
 // components
 import Navbar from '../../Components/Navbar'
+import Track from '../../Components/Track';
+import Loading from '../../Components/Loading';
 
 function Artists() {
+  
+  const { id } = useParams()
+  const [artist, setArtist] = useState(null)
+  const [tracks, setTracks] = useState(false)
+
+  console.log(tracks)
+
+  useEffect(()=>{
+      fetch(`https://deezerdevs-deezer.p.rapidapi.com/artist/${id}`, {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com',
+          'X-RapidAPI-Key': '2586c485fcmshc691be2f540fc1dp133582jsna421e997d391'
+        }
+      })
+      .then(response => response.json())
+      .then(response => {
+        setArtist(response)
+        getTracks(response)
+      })
+      .catch(err => console.error(err));
+  }, [id])
+
+  function getTracks(informations){
+    fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${informations.name}&index=0&limit=100`, {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+        "x-rapidapi-key": "2586c485fcmshc691be2f540fc1dp133582jsna421e997d391"
+      }})
+      .then(response => response.json())
+      .then(response => setTracks(response.data))
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
   return (
       <Container>
         <Navbar/>
 
         <MainContentContainer>
           <ArtistInformationsContainer>
-
+            <ArtistImageContainer>
+              <ArtistImage 
+                src={artist ? artist.picture_xl || artist.picture_big : ''}
+              />
+              {artist ? (
+              <ArtistImageShadow />
+              ) : ''}
+            </ArtistImageContainer>
+            <ArtistNameContainer>
+                <span>{artist ? artist.name : ''}</span>
+            </ArtistNameContainer>
           </ArtistInformationsContainer>
 
           <TracksContainer>
-          
+            <TrackList>
+                {
+                  tracks ? tracks.map( item => {
+                     return (<li key={item.id}>
+                      <Track 
+                        track={item}
+                        titleContainerWidth={400}
+                        interactiveContainerWidth={150}
+                        reproduction={false}
+                      />
+                    </li>)
+                  }) : 
+                  <Loading />
+                }
+            </TrackList>
           </TracksContainer>
         </MainContentContainer>
       </Container>
--- src/Pages/Artist/styles.js
+++ src/Pages/Artist/styles.js
@@ -46,10 +46,76 @@ export const ArtistInformationsContainer = styled.div`
   width: 350px;
   height: calc(100% - 80px);
   margin-left: 120px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const ArtistImageContainer = styled.div`
+  width: 90%;
+  height: 400px;
+  border-radius: 40px;
+  position: relative;
+`;
+
+export const ArtistImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 40px;
+  position: absolute;
+  border: none;
+`;
+
+export const ArtistImageShadow = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  border-radius: 40px;
+  background: linear-gradient(90deg, rgba(0,0,0,0.6), rgba(0,0,0,0.2));
+`;
+
+export const ArtistNameContainer = styled.div`
+  width: 90%;
+  height: 65px;
+  margin-top: 5%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  & span {
+    font-size: 22px;
+    color: white;
+    font-weight: bold;
+    white-space: nowrap;
+    text-align: center;
+    overflow: hidden;
+  }
 `;
 
 export const TracksContainer = styled.div`
   width: calc(100% - 150px - 500px);
   height: calc(100% - 80px);
   margin-right: 120px;
+  background-color: var(--color-special-background);
+  padding: 20px;
+`;
+
+export const TrackList = styled.ul`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  overflow-y: scroll;
+  overflow-x: hidden;
+
+  &::-webkit-scrollbar {
+    width: 6px;
+    background-color: var(--color-special-background);
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: var(--color-text-special);
+  }
 `;
